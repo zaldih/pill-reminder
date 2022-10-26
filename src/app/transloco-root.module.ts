6ef75124a7d5ceb96eba56af3ed89f8fa120182d
albagala <albagala20@gmail.com>
2022-10-26 21:37:05 +0200

    feat(i18n): add fr translation

--- src/app/transloco-root.module.ts
+++ src/app/transloco-root.module.ts
@@ -25,7 +25,7 @@ export class TranslocoHttpLoader implements TranslocoLoader {
     {
       provide: TRANSLOCO_CONFIG,
       useValue: translocoConfig({
-        availableLangs: ['en', 'es'],
+        availableLangs: ['en', 'es', 'fr'],
         defaultLang: 'es',
         // Remove this option if your application doesn't support changing language in runtime.
         reRenderOnLangChange: true,
